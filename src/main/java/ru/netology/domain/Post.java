6acba05c686290e4dcb52ed7981ef989c7265c91
Boris.Budenovsky <fpqb00@gmail.com>
2021-02-01 16:37:52 +0300

    initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,95 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private String groupImageUrl;
+    private String groupName;
+    private String timeOfPost;
+    private String textOfPost;
+    private String imageOfPostUrl;
+
+    private LikesInfo likesInfo;
+    private CommentsInfo commentsInfo;
+    private RepostsInfo repostsInfo;
+    private int numberOfViews;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGroupImageUrl() {
+        return groupImageUrl;
+    }
+
+    public void setGroupImageUrl(String groupImageUrl) {
+        this.groupImageUrl = groupImageUrl;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getTimeOfPost() {
+        return timeOfPost;
+    }
+
+    public void setTimeOfPost(String timeOfPost) {
+        this.timeOfPost = timeOfPost;
+    }
+
+    public String getTextOfPost() {
+        return textOfPost;
+    }
+
+    public void setTextOfPost(String textOfPost) {
+        this.textOfPost = textOfPost;
+    }
+
+    public String getImageOfPostUrl() {
+        return imageOfPostUrl;
+    }
+
+    public void setImageOfPostUrl(String imageOfPostUrl) {
+        this.imageOfPostUrl = imageOfPostUrl;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public RepostsInfo getRepostsInfo() {
+        return repostsInfo;
+    }
+
+    public void setRepostsInfo(RepostsInfo repostsInfo) {
+        this.repostsInfo = repostsInfo;
+    }
+
+    public int getNumberOfViews() {
+        return numberOfViews;
+    }
+
+    public void setNumberOfViews(int numberOfViews) {
+        this.numberOfViews = numberOfViews;
+    }
+}
