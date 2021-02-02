2df9dc4815074853862deb50d9571385497cb463
Boris.Budenovsky <fpqb00@gmail.com>
2021-02-02 18:35:44 +0300

    added classes

---
+++ src/main/java/ru/netology/domain/Additionally.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Additionally {
+    private String complain;
+    private String addToFavorite;
+
+    public String getComplain() {
+        return complain;
+    }
+
+    public void setComplain(String complain) {
+        this.complain = complain;
+    }
+
+    public String getAddToFavorite() {
+        return addToFavorite;
+    }
+
+    public void setAddToFavorite(String addToFavorite) {
+        this.addToFavorite = addToFavorite;
+    }
+}
