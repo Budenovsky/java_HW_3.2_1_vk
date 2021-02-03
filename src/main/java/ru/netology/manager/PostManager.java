1c48020520d0b157e7a2945bf123006fe3cbc5a4
Boris.Budenovsky <fpqb00@gmail.com>
2021-02-03 19:18:26 +0300

    added postManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] getSearch(int ownerId, String query, boolean ownersOnly, int count, int offset) {
+
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {}
+
+}
